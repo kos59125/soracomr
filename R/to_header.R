cf49952d0bad60d3ee96e57b93ec3fc76756b299
kos59125 <mail@recyclebin.jp>
2015-11-22 03:04:52 +0900

    Fix documentation

--- R/to_header.R
+++ R/to_header.R
@@ -4,7 +4,7 @@
 #'
 #' @param token
 #'    API Token.
-#' @param lang
+#' @param language
 #'    Two-letter language.
 to_headers <- function(token, language = getOption("soracomr.language")) {
    c(
